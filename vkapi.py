d893d619b0194e3d68bea3279da07d7acfcc0823
Ereduvyge <35508222+Ereduvyge@users.noreply.github.com>
2018-11-19 09:32:35 +0000

    Update vkapi.py

--- vkapi.py
+++ vkapi.py
@@ -30,16 +30,41 @@ while 1:
                 print ('Sent!')
                 vk_api.messages.send(user_ids=yourLove, message=messageTo, domain=yourLove)
 
+    def check(perid,has):
+        if has==1:
+            try:
+                mesid=vk_api.messages.send(user_ids=perid, peer_id=perid, message='1')
+                time.sleep(1)
+                vk_api.messages.delete(message_ids=mesid, delete_for_all=1)
+                return True
+            except:
+                return False
+        else:
+            try:
+                mesid=vk_api.messages.send(user_ids=perid, domain=perid, message='1')
+                time.sleep(1)
+                vk_api.messages.delete(message_ids=mesid, delete_for_all=1)
+                return True
+            except:
+                return False
+                
+                
     while len(ids_list)<3:
+        hasdom=0
         if re.match(r'^http',pyperclip.paste()):
             yourLove=pyperclip.paste()
             if re.match(r'id', yourLove):
                 yourLove=yourLove.split('d')[1]
+                hasdom=1
             else:
                 yourLove=yourLove.split('com/')[1]
+                hasdom=0
             if yourLove not in ids_list:
-                print(yourLove)
-                ids_list.append(yourLove)
+                if check(yourLove,hasdom)==True:
+                    print(yourLove)
+                    ids_list.append(yourLove)
+                else:
+                    time.sleep(2)
             else:
                 time.sleep(2)
         else:
