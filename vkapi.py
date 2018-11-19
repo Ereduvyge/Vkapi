d98172e8c78e3b3eda2795000c4f04f5764cbbe9
Ereduvyge <35508222+Ereduvyge@users.noreply.github.com>
2018-11-19 08:28:11 +0300

    Update vkapi.py

--- vkapi.py
+++ vkapi.py
@@ -3,41 +3,57 @@
 import vk
 import unicodedata
 import re
+import pyperclip
+import time
+
+while 1:
+    app_id='6755953'
+    login='89118236470'
+    password='R1HWO62FMyPNftWmiCDnhxd6wb38skCeqebreTNf'
+
+    session=vk.AuthSession(app_id, login, password, scope='friends, messages')
+    vk_api=vk.API(session, v='5.87', lang='ru')
+
+    ids_list=[]
+
+    def sent(your,message):
+        yourLove=your
+        messageTo=message
+        if re.match(r'^http', yourLove):
+            yourLove=yourLove.split('com/')[1]
+        if re.match(r'id', yourLove):
+            yourLove=yourLove.split('d')[1]
+            for i in vk_api.users.get(user_ids=yourLove):
+                print ('Sent!')
+                vk_api.messages.send(user_ids=yourLove, message=messageTo, peer_id=yourLove)
+        else:
+            for i in vk_api.users.get(user_ids=yourLove):
+                print ('Sent!')
+                vk_api.messages.send(user_ids=yourLove, message=messageTo, domain=yourLove)
+
+    while len(ids_list)<3:
+        if re.match(r'^http',pyperclip.paste()):
+            yourLove=pyperclip.paste()
+            if re.match(r'id', yourLove):
+                yourLove=yourLove.split('d')[1]
+            else:
+                yourLove=yourLove.split('com/')[1]
+            if yourLove not in ids_list:
+                print(yourLove)
+                ids_list.append(yourLove)
+            else:
+                time.sleep(2)
+        else:
+            time.sleep(2)
+
+    messageTo=input('Enter message:\n->')
 
 
-app_id='6752155'
-login='89118236470'
-password='R1HWO62FMyPNftWmiCDnhxd6wb38skCeqebreTNf'
+    for love in ids_list:
+        loh=vk_api.users.get(user_ids=love)[0]
+        #name = u''.join(loh['first_name']).encode('utf-8').strip()
+        messageTo='%s, '% loh['first_name'] + messageTo
+        print(messageTo)
+        sent(love,messageTo)
 
-session=vk.AuthSession(app_id, login, password, scope='friends, messages')
-vk_api=vk.API(session, v='5.87', lang='ru')
-
-ids_list=[]
-
-def sent(your,message):
-    yourLove=your
-    messageTo=message
-    if re.match(r'^http', yourLove): 
-        yourLove=yourLove.split('com/')[1]
-    if re.match(r'id', yourLove):
-        yourLove=yourLove.split('d')[1]
-        for i in vk_api.users.get(user_ids=yourLove):
-            print 'Sent!'
-            vk_api.messages.send(user_ids=yourLove, message=messageTo, peer_id=yourLove)
-    else:
-        for i in vk_api.users.get(user_ids=yourLove):
-            print 'Sent!'
-            vk_api.messages.send(user_ids=yourLove, message=messageTo, domain=yourLove)
-
-while len(ids_list)<1:
-    yourLove=raw_input('Enter id or domain or link:\n->')
-    ids_list.append(yourLove)
-
-messageTo=raw_input('Enter message:\n->')
-
-
-for love in ids_list:
-    loh=vk_api.users.get(user_ids=love)[0]
-    name = u''.join(loh['first_name']).encode('utf-8').strip()
-    messageTo='%s, '% name + messageTo
-    print messageTo 
+exit=input()
